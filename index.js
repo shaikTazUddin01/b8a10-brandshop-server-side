32b4c95538466e6684df5d9ec07d4e9d7a028eb1
sheikhtazuddin <tazahmedsoft@gmail.com>
2023-10-21 12:38:16 +0600

    rechecked and solved server deploy related problem

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
-const port = process.env.PORT || 5000
 require('dotenv').config()
+const port = process.env.PORT || 5000
 //middleware
 app.use(express.json())
-app.use(cors())
+const corsConfig = {
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT',"PATCH",'DELETE']
+    }
+    app.use(cors(corsConfig))
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.25fgudl.mongodb.net/?retryWrites=true&w=majority`;
@@ -21,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // create connection dataBase and collection;
         const newBrandDB = client.db('BrandDB').collection('Brand')
         const OrderDB = client.db('BrandDB').collection('Order')
@@ -58,7 +63,7 @@ async function run() {
             console.log(query)
             const filterProduct = await newBrandDB.find(query).toArray();
             res.send(filterProduct);
-            console.log(filterProduct)
+            // console.log(filterProduct)
         });
 
         app.put('/products/:id', async (req, res) => {
@@ -107,8 +112,8 @@ async function run() {
 
         })
 
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -116,7 +121,7 @@ async function run() {
 }
 run().catch(console.dir);
 app.get('/', (req, res) => {
-    res.send('this is taz')
+    res.send('server is connected')
 })
 
 
